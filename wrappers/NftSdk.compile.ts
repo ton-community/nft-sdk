94e6e729359e7766b794c56da1b8cbc46f379809
Gajesh Naik <robogajesh@gmail.com>
2023-04-07 14:00:55 +0400

    Revert "nft standard wrappers ready"
    This reverts commit 122efd96562f2482412206b951f86859c246a7ff.

--- wrappers/NftSdk.compile.ts
+++
@@ -1,6 +0,0 @@
-import { CompilerConfig } from '@ton-community/blueprint';
-
-export const compile: CompilerConfig = {
-    lang: 'func',
-    targets: ['contracts/imports/stdlib.fc', 'contracts/op-codes.fc', 'contracts/params.fc', 'contracts/nft_sdk.fc'],
-};
