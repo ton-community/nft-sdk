287a3c290e5b3985ef15b74b9bc92d9b41bc83ce
Gajesh Naik <robogajesh@gmail.com>
2023-04-10 13:55:21 +0400

    NftCollectionEditable

---
+++ wrappers/NftCollectionEditable.ts
@@ -0,0 +1,68 @@
+import { Address, beginCell, Cell, Contract, ContractProvider, Sender, SendMode, Slice } from 'ton-core';
+
+export class NftCollectionEditable implements Contract {
+    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+
+    static createFromAddress(address: Address) {
+        return new NftCollectionEditable(address);
+    }
+
+    // const { stack } = await provider.get('get_nft_address_by_index', [
+    //     { type: 'int', value: index }
+    // ])
+
+    async getCollectionData(
+        provider: ContractProvider, 
+        next_item_index: bigint, 
+        collection_content: Cell,
+        owner_address: Slice
+    ) {
+        const { stack } = await provider.get('get_collection_data', [
+            { type: 'int', value: next_item_index },
+            { type: 'cell', cell: collection_content },
+            { type: 'slice', cell: owner_address.asCell() }
+        ])
+        return {
+            next_item_index: stack.readBigNumber(),
+            collection_content: stack.readCellOpt(),
+            owner_address: stack.readAddressOpt(),
+        }
+    }
+
+    async getNftAddressByIndex(
+        provider: ContractProvider,
+        index: bigint
+    ) {
+        const { stack } = await provider.get('get_nft_address_by_index', [
+            { type: 'int', value: index }
+        ])
+        return {
+            nft_address: stack.readAddressOpt(),
+        }
+    }
+
+    async getNftContent(
+        provider: ContractProvider,
+        index: bigint,
+        individual_content: Cell
+    ) {
+        const { stack } = await provider.get('get_nft_content', [
+            { type: 'int', value: index },
+            { type: 'cell', cell: individual_content }
+        ])
+        return {
+            full_content: stack.readCellOpt(),
+        }
+    }
+
+    async getRoyaltyParams(
+        provider: ContractProvider
+    ) {
+        const { stack } = await provider.get('royalty_params', [])
+        return {
+            numerator: stack.readBigNumber(),
+            denominator: stack.readBigNumber(),
+            destination: stack.readAddressOpt()
+        }
+    }
+}
