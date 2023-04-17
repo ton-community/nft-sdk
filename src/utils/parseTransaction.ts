703cedcba978b757a2e2ae8c6cadba7daf4785b5
Gajesh Naik <robogajesh@gmail.com>
2023-04-17 17:55:12 +0400

    encode-decode as per ton-core style

--- src/utils/parseTransaction.ts
+++ src/utils/parseTransaction.ts
@@ -1,4 +1,4 @@
-import { loadTransaction, Slice, Address } from "ton-core";
+import { loadTransaction, Slice, Address, Cell } from "ton-core";
 
 export type Info = {
     type: "transfer" | "royalty_params" | "unknown" | null;
@@ -20,6 +20,28 @@ export type RoyaltyInfo = {
     queryId: number | undefined;
     from: Address | undefined;
     nftItem: Address | undefined;
+    value: bigint | undefined;
+};
+
+export type EditContentInfo = {
+    queryId: number | undefined;
+    from: Address | undefined;
+    nftItem: Address | undefined;
+    payload: Cell | undefined;
+    value: bigint | undefined;
+};
+
+// transfer_editorship#1c04412a query_id:uint64 new_editor:MsgAddress response_destination:MsgAddress custom_payload:(Maybe ^Cell)  forward_amount:(VarUInteger 16) forward_payload:(Either Cell ^Cell) = EditRequest;
+
+export type TransferEditorshipInfo = {
+    queryId: number | undefined;
+    from: Address | undefined;
+    nftItem: Address | undefined;
+    newEditor: Address | undefined;
+    responseDestination: Address | undefined;
+    customPayload?: Cell | null;
+    forwardAmount?: bigint;
+    forwardPayload?: Cell | null;
     value: bigint | undefined;
 };
 
@@ -95,6 +117,14 @@ export function parseNftTransferInfo(
     const tx = loadTransaction(transaction);
     const body = tx.inMessage?.body.beginParse();
     
+    let op;
+
+    try {
+        op = body?.loadUint(32);
+    } catch {
+        return null;
+    }
+
     if (
         tx.inMessage?.info.type == "internal" 
         && tx.description.type == "generic" 
@@ -123,19 +153,97 @@ export function parseRoyaltyInfo(
     const tx = loadTransaction(transaction);
     const body = tx.inMessage?.body.beginParse();
     
+    let op;
+
+    try {
+        op = body?.loadUint(32);
+    } catch {
+        return null;
+    }
+
     if (
         tx.inMessage?.info.type == "internal" 
         && tx.description.type == "generic" 
         && tx.description.computePhase.type == "vm"
     ) {
-        const nftTransfer: RoyaltyInfo = {
+        const royaltyInfo: RoyaltyInfo = {
             queryId: body?.loadUint(64),
             from: tx.inMessage?.info.src,
             nftItem: tx.inMessage?.info.dest,
             value: tx.inMessage?.info.value.coins,
         };
 
-        return nftTransfer;
+        return royaltyInfo;
+    }
+
+    return null;
+}
+
+export function parseEditContentInfo(
+    transaction: Slice,
+): EditContentInfo | null {
+    const tx = loadTransaction(transaction);
+    const body = tx.inMessage?.body.beginParse();
+
+    let op;
+
+    try {
+        op = body?.loadUint(32);
+    } catch {
+        return null;
+    }
+    
+    if (
+        tx.inMessage?.info.type == "internal" 
+        && tx.description.type == "generic" 
+        && tx.description.computePhase.type == "vm"
+    ) {
+        const editContentInfo: EditContentInfo = {
+            queryId: body?.loadUint(64),
+            from: tx.inMessage?.info.src,
+            nftItem: tx.inMessage?.info.dest,
+            payload: body?.loadRef(), // TODO: get exact info
+            value: tx.inMessage?.info.value.coins,
+        };
+
+        return editContentInfo;
+    }
+
+    return null;
+}
+
+export function parseTransferEditorshipInfo(
+    transaction: Slice,
+): TransferEditorshipInfo | null {
+    const tx = loadTransaction(transaction);
+    const body = tx.inMessage?.body.beginParse();
+    
+    let op;
+
+    try {
+        op = body?.loadUint(32);
+    } catch {
+        return null;
+    }
+
+    if (
+        tx.inMessage?.info.type == "internal" 
+        && tx.description.type == "generic" 
+        && tx.description.computePhase.type == "vm"
+    ) {
+        const transferEditorshipInfo: TransferEditorshipInfo = {
+            queryId: body?.loadUint(64),
+            from: tx.inMessage?.info.src,
+            nftItem: tx.inMessage?.info.dest,
+            newEditor: body?.loadAddress(),
+            responseDestination: body?.loadAddress(),
+            customPayload: body?.loadMaybeRef(),
+            forwardAmount: body?.loadCoins(),
+            forwardPayload: body?.loadMaybeRef(),
+            value: tx.inMessage?.info.value.coins,
+        };
+
+        return transferEditorshipInfo;
     }
 
     return null;
