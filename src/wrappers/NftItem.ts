be1771734b15dbd1dfbf3812da7dbb38cd01ab19
Gajesh Naik <robogajesh@gmail.com>
2023-04-20 17:04:14 +0400

    add more functions to NftItem

--- src/wrappers/NftItem.ts
+++ src/wrappers/NftItem.ts
@@ -7,11 +7,87 @@ export class NftItem implements Contract {
         return new NftItem(address);
     }
 
-    async sendTrans 
+    async sendTransfer(provider: ContractProvider, via: Sender, params: {
+        value: bigint
+        queryId: bigint
+        newOwner: Address
+        responseDestination: Address
+        customPayload?: Cell
+        forwardAmount: bigint
+        forwardPayload?: Cell
+    }) {
+        await provider.internal(via, {
+            value: params.value,
+            body: beginCell()
+                .storeUint(0x5fcc3d14, 32)
+                .storeUint(params.queryId, 64)
+                .storeAddress(params.newOwner)
+                .storeAddress(params.responseDestination)
+                .storeMaybeRef(params.customPayload)
+                .storeCoins(params.forwardAmount)
+                .storeMaybeRef(params.forwardPayload)
+                .endCell(),
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+        })
+    }
 
-    // const { stack } = await provider.get('get_nft_address_by_index', [
-    //     { type: 'int', value: index }
-    // ])
+    async sendGetStaticData(
+        provider: ContractProvider,
+        via: Sender,
+        params: {
+            value: bigint
+            queryId: bigint
+        }
+    ) {
+        await provider.internal(via, {
+            value: params.value,
+            body: beginCell()
+                .storeUint(0x2fcb26a2, 32)
+                .storeUint(params.queryId || 0, 64)
+                .endCell(),
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+        })
+    }
+
+    async sendGetRoyaltyParams(
+        provider: ContractProvider,
+        via: Sender,
+        params: {
+            value: bigint
+            queryId: bigint
+        }
+    ) {
+        await provider.internal(via, {
+            value: params.value,
+            body: beginCell()
+                .storeUint(0x693d3950, 32)
+                .storeUint(params.queryId, 64)
+                .endCell(),
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+        })
+    }
+
+    async sendTransferEditorship(provider: ContractProvider, via: Sender, params: { 
+        value: bigint, 
+        queryId?: number,
+        newEditor: Address, 
+        responseTo: Address|null,
+        forwardAmount?: bigint 
+    }) {
+        await provider.internal(via, {
+            value: params.value,
+            body: beginCell()
+                .storeUint(0x1c04412a, 32)
+                .storeUint(params.queryId || 0, 64)
+                .storeAddress(params.newEditor)
+                .storeAddress(params.responseTo)
+                .storeBit(false)
+                .storeCoins(params.forwardAmount || 0)
+                .storeBit(false)
+                .endCell(),
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+        })
+    }
 
     async getNftData(provider: ContractProvider) {
         const { stack } = await provider.get('get_nft_data', [])
