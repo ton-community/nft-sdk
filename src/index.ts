ea401212854c282eebe621b698235c5ba6af109d
Gajesh Naik <robogajesh@gmail.com>
2023-04-30 11:24:31 +0400

    generated docs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import {AmazonS3} from "./storage/AmazonS3";
 import {Pinata} from "./storage/Pinata";
 
-export * from "./wrappers/standard/NftCollection";
-export * from "./wrappers/standard/NftItem";
+export * from "./wrappers/getgems/NftCollection";
+export * from "./wrappers/getgems/NftItem";
 export * from "./wrappers/getgems/SbtSingle";
 export * from "./wrappers/standard/NftItemRoyalty";
-
+export * from "./wrappers/getgems/NftAuction";
+export * from "./wrappers/getgems/NftAuctionV2";
+export * from "./wrappers/getgems/NftFixedPrice";
+export * from "./wrappers/getgems/NftFixedPriceV2";
+export * from "./wrappers/getgems/NftFixedPriceV3";
+export * from "./wrappers/getgems/NftMarketplace";
+export * from "./wrappers/getgems/NftOffer";
+export * from "./wrappers/getgems/NftSwap";
 
 export {
     AmazonS3, 
