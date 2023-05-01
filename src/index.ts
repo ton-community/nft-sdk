422d9ac20a49202b94017092a031d5afe67ff9ac
Gajesh Naik <robogajesh@gmail.com>
2023-05-01 16:53:10 +0400

    docs update

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@ export * from "./types/OffchainContent"
 
 // Transaction Parsing
 export * as TransactionParsing from "./transaction-parsing/"
+export * from "./utils/FetchAndParseTransaction";
 
 export {
     AmazonS3, 
