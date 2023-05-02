6ccb093ec879b940839938e43fc514368c336c33
Gajesh Naik <robogajesh@gmail.com>
2023-05-02 17:07:17 +0400

    uncommented and fixed stuff

--- src/types/OffchainContent.ts
+++ src/types/OffchainContent.ts
@@ -1,5 +1,5 @@
 import { beginCell, Builder } from "ton-core";
-import { Cell } from "ton-core";
+import { Cell } from "ton";
 import { Slice } from "ton";
 
 // offchain#01 uri:Text = FullContent;
@@ -10,20 +10,20 @@ export type Offchain = {
     uri: String,
 };
 
-// export function flattenSnakeCell(cell: Cell) {
-//     let c: Cell|null = cell
+export function flattenSnakeCell(cell: Cell) {
+    let c: Cell|null = cell
 
-//     let res = Buffer.alloc(0)
+    let res = Buffer.alloc(0)
 
-//     while (c) {
-//         let cs = c.beginParse()
-//         let data = cs.remainingBits()
-//         res = Buffer.concat([res, data])
-//         c = c.refs[0]
-//     }
+    while (c) {
+        let cs = c.beginParse()
+        let data = cs.readRemainingBytes()
+        res = Buffer.concat([res, data])
+        c = c.refs[0]
+    }
 
-//     return res
-// }
+    return res
+}
 
 function bufferToChunks(buff: Buffer, chunkSize: number) {
     let chunks: Buffer[] = []
@@ -54,29 +54,29 @@ export function makeSnakeCell(data: Buffer) {
     return rootCell
 }
 
-// export function loadOffchainContent(content: Cell): Offchain {
-//     let data = flattenSnakeCell(content)
+export function loadOffchainContent(content: Cell): Offchain {
+    let data = flattenSnakeCell(content)
 
-//     let prefix = data[0]
-//     if (prefix !== OFF_CHAIN_CONTENT_PREFIX) {
-//         throw new Error(`Unknown content prefix: ${prefix.toString(16)}`)
-//     }
-//     return {
-//         uri: data.slice(1).toString()
-//     }
-// }
+    let prefix = data[0]
+    if (prefix !== OFF_CHAIN_CONTENT_PREFIX) {
+        throw new Error(`Unknown content prefix: ${prefix.toString(16)}`)
+    }
+    return {
+        uri: data.slice(1).toString()
+    }
+}
 
-// export function storeOffchainContent(content: Offchain) {
-//     let data = Buffer.from(content.uri)
-//     let offChainPrefix = Buffer.from([OFF_CHAIN_CONTENT_PREFIX])
-//     data = Buffer.concat([offChainPrefix, data])
+export function storeOffchainContent(content: Offchain) {
+    let data = Buffer.from(content.uri)
+    let offChainPrefix = Buffer.from([OFF_CHAIN_CONTENT_PREFIX])
+    data = Buffer.concat([offChainPrefix, data])
 
-//     return (builder: Builder) => {
-//         builder.storeRef(
-//             makeSnakeCell(data)
-//         );
-//     };
-// }
+    return (builder: Builder) => {
+        builder.storeRef(
+            makeSnakeCell(data)
+        );
+    };
+}
 
 export function encodeOffChainContent(content: string) {
     let data = Buffer.from(content)
