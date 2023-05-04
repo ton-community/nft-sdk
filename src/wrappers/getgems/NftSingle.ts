4c9ecd6ec77475ae666a355c9b09eae4c10e2cb3
Gajesh Naik <robogajesh@gmail.com>
2023-05-04 17:24:33 +0400

    removed combineFunc

--- src/wrappers/getgems/NftSingle.ts
+++ src/wrappers/getgems/NftSingle.ts
@@ -1,6 +1,5 @@
 import { Address, beginCell, Cell, Contract, ContractProvider, Sender, SendMode, contractAddress } from 'ton-core';
 import { encodeOffChainContent } from '../../types/OffchainContent';
-import { combineFunc } from '../../utils/combineFunc';
 
 export class NftSingle implements Contract {
     constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
@@ -175,13 +174,6 @@ export function buildeNftSingleDataCell(data: NftSingleData) {
 }
 
 // Data
-
-export const NftSingleSource = combineFunc(__dirname, [
-    '../../sources/stdlib.fc',
-    '../../sources/op-codes.fc',
-    '../../sources/params.fc',
-    '../../sources/nft-single.fc',
-])
 
 export const NftSingleCodeBoc = 'te6cckECFQEAAwoAART/APSkE/S88sgLAQIBYgcCAgEgBAMAI7x+f4ARgYuGRlgOS/uAFoICHAIBWAYFABG0Dp4AQgRr4HAAHbXa/gBNhjoaYfph/0gGEAICzgsIAgEgCgkAGzIUATPFljPFszMye1UgABU7UTQ+kD6QNTUMIAIBIA0MABE+kQwcLry4U2AEuQyIccAkl8D4NDTAwFxsJJfA+D6QPpAMfoAMXHXIfoAMfoAMPACBtMf0z+CEF/MPRRSMLqOhzIQRxA2QBXgghAvyyaiUjC64wKCEGk9OVBSMLrjAoIQHARBKlIwuoBMSEQ4BXI6HMhBHEDZAFeAxMjQ1NYIQGgudURK6n1ETxwXy4ZoB1NQwECPwA+BfBIQP8vAPAfZRNscF8uGR+kAh8AH6QNIAMfoAggr68IAboSGUUxWgod4i1wsBwwAgkgahkTbiIML/8uGSIY4+ghBRGkRjyFAKzxZQC88WcSRKFFRGsHCAEMjLBVAHzxZQBfoCFctqEssfyz8ibrOUWM8XAZEy4gHJAfsAEFeUECo4W+IQAIICjjUm8AGCENUydtsQN0UAbXFwgBDIywVQB88WUAX6AhXLahLLH8s/Im6zlFjPFwGRMuIByQH7AJMwMzTiVQLwAwBUFl8GMwHQEoIQqMsArXCAEMjLBVAFzxYk+gIUy2oTyx/LPwHPFsmAQPsAAIYWXwZsInDIywHJcIIQi3cXNSHIy/8D0BPPFhOAQHCAEMjLBVAHzxZQBfoCFctqEssfyz8ibrOUWM8XAZEy4gHJAfsAAfZRN8cF8uGR+kAh8AH6QNIAMfoAggr68IAboSGUUxWgod4i1wsBwwAgkgahkTbiIMIA8uGSIY4+ghAFE42RyFALzxZQC88WcSRLFFRGwHCAEMjLBVAHzxZQBfoCFctqEssfyz8ibrOUWM8XAZEy4gHJAfsAEGeUECo5W+IUAIICjjUm8AGCENUydtsQN0YAbXFwgBDIywVQB88WUAX6AhXLahLLH8s/Im6zlFjPFwGRMuIByQH7AJMwNDTiVQLwA+GNLv4='
 
--- src/wrappers/standard/NftItem.ts
+++ src/wrappers/standard/NftItem.ts
@@ -1,5 +1,4 @@
 import { Address, beginCell, Cell, Contract, ContractProvider, Sender, SendMode, contractAddress, storeStateInit, toNano } from 'ton-core';
-import { combineFunc } from '../../utils/combineFunc';
 import { encodeOffChainContent } from '../../types/OffchainContent';
 import {compile} from "@ton-community/blueprint";
 import { StateInit } from 'ton';
@@ -80,13 +79,6 @@ export function buildNftItemDataCell(data: NftItemData) {
 
     return dataCell.endCell()
 }
-
-export const NftItemSource = combineFunc(__dirname, [
-    '../../sources/stdlib.fc',
-    '../../sources/op-codes.fc',
-    '../../sources/params.fc',
-    '../../sources/nft-single.fc',
-])
 
 export const NftItemCodeBoc = 'te6ccgECFQEAAw4AART/APSkE/S88sgLAQIBYgIDAgLOBAUCASAREgIBIAYHAgEgDxAEvQyIccAkl8D4NDTAwFxsJJfA+D6QPpAMfoAMXHXIfoAMfoAMPACBtMf0z+CEF/MPRRSMLqOiTIQRxA2QBXbPOCCEC/LJqJSMLrjAoIQaT05UFIwuuMCghAcBEEqUjC6gCAkKCwARPpEMHC68uFNgAfZRN8cF8uGR+kAh8AH6QNIAMfoAggr68IAboSGUUxWgod4i1wsBwwAgkgahkTbiIMIA8uGSIY4+ghAFE42RyFALzxZQC88WcSRLFFRGwHCAEMjLBVAHzxZQBfoCFctqEssfyz8ibrOUWM8XAZEy4gHJAfsAEGeUECo5W+IMAIYWXwZsInDIywHJcIIQi3cXNSHIy/8D0BPPFhOAQHCAEMjLBVAHzxZQBfoCFctqEssfyz8ibrOUWM8XAZEy4gHJAfsAAFQWXwYzAdASghCoywCtcIAQyMsFUAXPFiT6AhTLahPLH8s/Ac8WyYBA+wABYI6JMhBHEDZAFds84DEyNDU1ghAaC51RErqfURPHBfLhmgHU1DAQI/AD4F8EhA/y8A0AggKONSbwAYIQ1TJ22xA3RgBtcXCAEMjLBVAHzxZQBfoCFctqEssfyz8ibrOUWM8XAZEy4gHJAfsAkzA0NOJVAvADAfZRNscF8uGR+kAh8AH6QNIAMfoAggr68IAboSGUUxWgod4i1wsBwwAgkgahkTbiIML/8uGSIY4+ghBRGkRjyFAKzxZQC88WcSRKFFRGsHCAEMjLBVAHzxZQBfoCFctqEssfyz8ibrOUWM8XAZEy4gHJAfsAEFeUECo4W+IOAIICjjUm8AGCENUydtsQN0UAbXFwgBDIywVQB88WUAX6AhXLahLLH8s/Im6zlFjPFwGRMuIByQH7AJMwMzTiVQLwAwAVO1E0PpA+kDU1DCAAGzIUATPFljPFszMye1UgAgFYExQAI7x+f4ARgYuGRlgOS/uAFoICHAAdtdr+AE2GOhph+mH/SAYQABG0Dp4AQgRr4HA='
 
